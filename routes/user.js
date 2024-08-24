48b137607ce1a3ace8258d7f995e68b6880d5af1
Your GitHub Username <your.email@example.com>
2024-08-24 22:39:07 +0500

    check

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,8 @@ const { isAuthenticated } = require("../middlewares/isAuthenticated");
 
 const router = express.Router();
 
-router.route("/login").get(getAllUsers);
+// router.route("/login").get(getAllUsers);
+router.route("/login").get(login);
 router.route("/logout").get(logout);
 router.route("/my/complains").get(isAuthenticated, getMyComplains);
 router.route("/usercomplaints/:id").get(isAuthenticated, getUserComplains);
