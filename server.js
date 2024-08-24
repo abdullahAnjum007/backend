0803c5efed0d46e2a6f171896b7041a7a779a0af
Your GitHub Username <your.email@example.com>
2024-08-24 17:42:13 +0500

    server to app

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 const server = http.createServer(app);
 
-initializeIo(server); // Initialize Socket.IO with the server instance
+// initializeIo(server); // Initialize Socket.IO with the server instance
 
 app.use(cors());
 app.use(express.json());
@@ -26,7 +26,12 @@ app.use("/api/v1", complainRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1", exhibitRoutes);
 app.use("/api/v1", pushTokenRoutes);
-
+//
+// Root route
+app.get("/", (req, res) => {
+  res.json({ message: "Connected successfully!" });
+});
+//
 // Connect to MongoDB and start the server
 const PORT = process.env.PORT || 5000;
 const DB_URI = "mongodb://localhost:27017/complaints";
@@ -34,7 +39,7 @@ const DB_URI = "mongodb://localhost:27017/complaints";
 mongoose
   .connect(DB_URI)
   .then(() => {
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
